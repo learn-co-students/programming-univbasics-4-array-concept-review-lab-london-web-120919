ed108735a20888ca613fd82c1af3985e3efe00f9
Tommy Berman <tomy.berman1@gmail.com>
2019-11-28 14:21:11 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,20 +9,12 @@ end
 
 def find_max_value(array)
   x = 0
- array.length.times {|i|
-    if array[i] > x
-    x = array[i]
-    end
-  }
+ array.length.times {|i|   x = array[i] if array[i] > x  }
   x
 end
 
 def find_min_value(array)
   x = array[0]
- array.length.times {|i|
-    if array[i] < x
-    x = array[i]
-  end
-  }
+ array.length.times {|i|  x = array[i] if array[i] < x }
   x
 end
